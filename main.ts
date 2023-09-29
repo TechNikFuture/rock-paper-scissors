736f062ab6c0f50f6ff6dfd59e670b15fba52581
TechNikFuture <108083158+TechNikFuture@users.noreply.github.com>
2023-09-29 09:09:50 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,34 @@
+let Zufallszahl = 0
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Zufallszahl = randint(1, 3)
+    if (Zufallszahl == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (Zufallszahl == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    }
+    if (Zufallszahl == 3) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
